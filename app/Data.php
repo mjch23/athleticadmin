4aeefebdadfb2b705585354c22d9529c4b9585ac
Martin Jean-Charles <mjch23@me.com>
2018-10-08 16:25:47 -0300

    update ProductoPresupuesto

--- app/Data.php
+++ app/Data.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Data extends Model
 {
-     protected $table = "ProductoPresupuesto";
+     protected $table = "productopresupuesto";
      protected $primaryKey = 'id_pp';
      protected $fillable = ['id_presupuesto','id_producto','nombre_actividad','nombre_producto','cantidad_pp','precio_unitario_pp','precio_total_pp'];  //
 }
