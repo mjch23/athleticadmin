f27840f0236e1ee3fcd81916f3f0e8ee6d60ff06
mjch23 <mjch23@gmail.com>
2018-10-21 21:38:20 -0300

    cambio en el Modal I

--- resources/views/presupuesto/index.blade.php
+++ resources/views/presupuesto/index.blade.php
@@ -70,7 +70,7 @@
  							 <div class="modal-dialog" role="document">
    								 <div class="modal-content">
      								 <div class="modal-header">
-       									 <h5 class="modal-title" id="exampleModalCenterTitle"><i class="fas fa-exclamation-triangle text-danger"></i>		Atención!</h5>
+       									 <h5 class="modal-title" id="exampleModalCenterTitle">¡Atención!</h5>
      									   <button type="button" class="close" data-dismiss="modal" aria-label="Close">
         								  <span aria-hidden="true">&times;</span>
    									     </button>
