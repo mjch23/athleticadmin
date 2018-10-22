d3c8ca80ddcd90d5a32a734269d0924d77b6f984
Martin Jean-Charles <mjch23@me.com>
2018-10-22 16:22:51 -0300

    Update Maayusculas ProductoPresupuesto

--- app/Http/Controllers/PresupuestoController.php
+++ app/Http/Controllers/PresupuestoController.php
@@ -133,7 +133,7 @@ class PresupuestoController extends Controller
             ->orderBy('id_actividad')
             ->get(); 
 
-            $precio_presupuesto = DB::table('productopresupuesto')
+            $precio_presupuesto = DB::table('ProductoPresupuesto')
             ->where('id_presupuesto','=',$id)
             ->sum('precio_total_pp');
 
