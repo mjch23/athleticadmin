2aad8ed1108dd9cfc9d221faa3a02e8c14564b76
mjch23 <mjch23@gmail.com>
2019-07-30 20:35:28 -0300

    cambios Lore 30-07 v2

--- resources/views/quince.blade.php
+++ resources/views/quince.blade.php
@@ -40,6 +40,21 @@
   </head>
 
   <body id="page-top">
+    <!-- Navigation -->
+    <a class="menu-toggle rounded" href="#">
+      <i class="fas fa-bars"></i>
+    </a>
+    <nav id="sidebar-wrapper">
+      <ul class="sidebar-nav">
+        <li class="sidebar-brand">
+          <a class="js-scroll-trigger" href="#">Lorena Farías Fotografía</a>
+        </li>
+        <li class="sidebar-nav-item">
+          <a class="js-scroll-trigger" href="https://lorenafariasfotografia.com.ar">Inicio</a>
+        </li>
+      </ul>
+    </nav>
+
 
 <div class="container-fluid">
 
